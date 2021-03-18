419409773ba36c161b0c8947ca6466d50d9a5950
tomxscode <tomasbarrosg13@gmail.com>
2021-03-18 11:25:58 -0300

    obtener la hora seleccionada

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const
   hours = document.querySelector('#hours'),
-  busHour = document.querySelector('#busHour')
+  busHour = document.querySelector('#busHour'),
+  btn = document.querySelector('#calculate')
   ;
 
 var
   date = new Date(),
-  currentHour = date.getHours() + '' + date.getMinutes()
+  currentHour = date.getHours() + '' + date.getMinutes(),
+  selectedHour = parseInt(hours.value)
   ;
   
-console.log(currentHour);
+btn.onclick = function() {
+  console.log('works')
+}
