c76bb5da88064efc6254af4938fb8c3f9cc61ea3
tomxscode <tomasbarrosg13@gmail.com>
2021-03-19 21:54:53 -0300

    arreglado horas

--- app.js
+++ app.js
@@ -16,14 +16,18 @@ var
   ;
 if (currentMinutes.length == 1) {
   let tempMinutes = currentMinutes;
-  currentMinutes = parseInt('0' + tempMinutes);
+  currentMinutes = '0' + tempMinutes;
 } else if (actualHour.length == 1) {
   let tempHour = actualHour;
-  actualHour = parseInt('0' + tempHour);
+  actualHour = '0' + tempHour;
 }
-currentHour = parseInt(actualHour) + '' + parseInt(currentMinutes);
+currentHour = parseInt(actualHour + currentMinutes);
+
+console.log(currentHour, selectedHour);
 
 btn.onclick = function() {
+  var hoursRest = 0,
+      minutesRest = 0;
   console.log(currentHour, selectedHour);
   console.log(selHour, selectedMinutes, ' / ' , actualHour, currentMinutes);
   let nextBusMinutes = selectedHour - currentHour;
@@ -34,10 +38,13 @@ btn.onclick = function() {
     console.log('Faltan ', nextBusMinutes);
     for (let x = 1; x < nextBusMinutes; x++) {
       if (nextBusMinutes > 60) {
-        nextBusMinutes =- 60;
+        nextBusMinutes = nextBusMinutes - 60;
         console.log(nextBusMinutes, ', index: ', x);
+        hoursRest = x;
+        minutesRest = nextBusMinutes;
         if (nextBusMinutes < 60) { break; }
       }
     }
+    console.log('Queda(n): ', hoursRest, ' hora(s) y ', minutesRest, ' minutos.');
   }
 }
